8e0bd0f02443b06463c67a97f0078ae320487ce2
GeradMup <gerrym751@gmail.com>
2023-12-16 17:32:51 +0200

    Finished setting the main entry page

---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NMP_Quoting_System.ViewModels
+{
+    public class MainViewModel
+    {
+        public MainViewModel()
+        {
+            
+        }
+    }
+}
