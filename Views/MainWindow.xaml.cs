2560eeaa720806d4b8cbb14629d14a8c082486ae
GeradMup <gerrym751@gmail.com>
2023-12-16 18:38:16 +0200

    Now we are able to resize the screen successfully

--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -8,6 +8,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Runtime;
+using System.Runtime.InteropServices;
+using System.Net.Http.Headers;
+using System.Windows.Interop;
 
 namespace NMP_Quoting_System
 {
@@ -19,6 +23,34 @@ namespace NMP_Quoting_System
         public MainWindow()
         {
             InitializeComponent();
+            this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+            this.logoPanel.MaxWidth = SystemParameters.MaximizedPrimaryScreenWidth;
+
+        }
+
+
+        //The below code will us to move around the window, maximise it and minimize it when dragging it from the top.
+        //The include in square brackets simply allows us to use functions from the Windows operating system directly.
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessage(IntPtr hWnd, int wMsg, int wParam, int lParam);
+
+        //Event to be called whenever the user tries to drag the window around!
+        private void pnlControlBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            WindowInteropHelper helper = new WindowInteropHelper(this);
+            SendMessage(helper.Handle, 161, 2, 0);
+        }
+
+        /// <summary>
+        /// We use this function to ensure that the window has a recognizable border when resized. 
+        /// This will allow the window to display correctly when in full screen mode!
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Maximized) this.BorderThickness = new System.Windows.Thickness(8);
+            else this.BorderThickness = new System.Windows.Thickness(0);
         }
     }
 }
