222a9e0add0ebc2f38c42da447b14ac5f3317c98
GeradMup <gerrym751@gmail.com>
2024-01-08 21:04:26 +0200

    Ready to collaborate

---
+++ Models/GearboxSelectionModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace NMP_Quoting_System.Models
+{
+    public class ARWGearboxModel
+    {
+        public required double Rating { get; set; }
+        public required string Type { get; set; }
+        public required double Number { get; set; }
+        public required string Ratio { get; set; }
+        public required int Poles { get; set; }
+        public required double InitialRpm { get; set; }
+        public required double FinalRpm  { get; set; }
+        public required double Torque { get; set; }
+        public required int HollowShaftDiameter { get; set; }
+        public required double Fs { get; set; }
+    }
+}
